0732481901bd3786d571361be2362ad777ed70d4
gera <gera@gera>
2015-08-29 15:40:16 -0300

    Se agregan DataTypes de Productos y Cant_Individual

---
+++ src/lab01/Clases/DataProducto.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab01.Clases;
+
+/**
+ *
+ * @author gera
+ */
+public class DataProducto {
+    private String nombre;
+    private String descripcion;
+    private double precio;
+    private String imagen;
+
+    public DataProducto() {
+    }
+
+    public DataProducto(String nombre, String descripcion, double precio, String imagen) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.imagen = imagen;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+   
+}
