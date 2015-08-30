c6167f51616a635ed1c307be69d1a670fc53036e
martin <martin@martin-Lenovo-G480>
2015-08-29 21:31:08 -0300

    Se agrega la imagen al Ver Usuario

---
+++ src/lab01/Clases/DataCant_Individual.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab01.Clases;
+
+/**
+ *
+ * @author gera
+ */
+public class DataCant_Individual {
+    private int cantidad;
+
+    public DataCant_Individual() {
+    }
+    
+    public DataCant_Individual(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+    
+}
--- src/lab01/Clases/DataUser.java
+++ src/lab01/Clases/DataUser.java
@@ -40,7 +40,6 @@ public class DataUser {
         
     }
     
-    
     public String getNickname(){
         return this.nickname;
     }
