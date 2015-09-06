dbc8934875b0da5029198c2a3cf80db97af32572
martin <martin@martin-Lenovo-G480>
2015-09-06 11:26:53 -0300

    se agrega VerInfoProducto y ver producto.

--- src/lab01/Interfaces/VerInfoProd.java
+++ src/lab01/Interfaces/VerInfoProd.java
@@ -50,7 +50,7 @@ public class VerInfoProd extends javax.swing.JFrame {
         lblImagenProd = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         lblNomProd.setText("Nombre: ");
 
